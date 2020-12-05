f6787cab1cf82f331c839c66c2c54138a7ce2254
Mochizuki_Mk19 <Tsukumoyarei@gmail.com>
2020-12-05 23:21:16 +0100

    try to fix the autoupdater

--- ElectronicObserverUpdater/Utillity/ZipFileExtensions.cs
+++ ElectronicObserverUpdater/Utillity/ZipFileExtensions.cs
@@ -35,7 +35,7 @@ namespace ElectronicObserverUpdater.Utillity
 			var root = source.Entries[0]?.FullName;
 			foreach (ZipArchiveEntry entry in source.Entries)
 			{
-				if (entry.FullName != "ElectronicObserverUpdater.exe" || entry.FullName != "ElectronicObserverUpdater.exe.config" || entry.FullName != "ElectronicObserverUpdater.pdb")
+				if (entry.Name != "ElectronicObserverUpdater.exe" || entry.Name != "ElectronicObserverUpdater.exe.config" || entry.Name != "ElectronicObserverUpdater.pdb")
 				{
 					count++;
 				//Remove zip file Root directory from path.
